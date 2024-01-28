63b68b50a782b68247f3e68eccbae32e74b5a12d
er.beeps <bipinkhatiwada10@gmail.com>
2024-01-28 19:44:28 +0545

    fixes in class name error

--- database/migrations/2020_04_25_025441_create_primary_master_tables.php
+++ database/migrations/2020_04_25_025441_create_primary_master_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAppPrimaryMasterTables extends Migration
+class CreatePrimaryMasterTables extends Migration
 {
     /**
      * Run the migrations.
